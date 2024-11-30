fd083a8ff237a5a4b2409b6bef6629ae63f9a807
samcarter <sam.carter8@aol.com>
2024-11-30 02:22:33 +0100

    paddington

--- samcarter/paddington/convert2video.sh
+++ samcarter/paddington/convert2video.sh
@@ -8,7 +8,7 @@ pdftoppm -png -r 240 -progress paddington.pdf paddington
 # convert to video
 ffmpeg \
   -ss 00:00:00 -i paddington-%03d.png \
-  -ss 00:00:20 -i The_Adventure_of_Paddington_Marmalade_Song-Best_Youtubers_and_Tv_shows_for_kids-EHxUb5akz48.m4a \
+  -ss 00:00:24 -i The_Adventure_of_Paddington_Marmalade_Song-Best_Youtubers_and_Tv_shows_for_kids-EHxUb5akz48.m4a \
   -shortest paddington_raw.mp4
 
 # repair video
