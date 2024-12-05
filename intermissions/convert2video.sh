4793f593ef653c3f792b28d76e9b0892df5185f5
samcarter <sam.carter8@aol.com>
2024-12-05 23:02:23 +0100

    update scripts

--- intermissions/convert2video.sh
+++ intermissions/convert2video.sh
@@ -81,5 +81,6 @@ convert2video abbeyroad "Abbey Road"
 convert2video whitewaterride "White Water Ride"
 convert2video paddington "Paddington"
 convert2video lion "African Night"
+convert2video silentnight "Silent Night"
 convert2video finale "Finale"
 repeatconvert credits "./extravaganza-credits.pdf" 150
