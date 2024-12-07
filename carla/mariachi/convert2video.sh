abce4423362f429a5aa8420b0b555e5c7bbf4c6c
samcarter <sam.carter8@aol.com>
2024-12-07 19:50:09 +0100

    mariachi: added intermediate steps

--- carla/mariachi/convert2video.sh
+++ carla/mariachi/convert2video.sh
@@ -2,8 +2,8 @@
 rm mariachi.mp4
 
 # convert to png images
-pdftoppm -png -r 48 -progress -singlefile -f 14 mariachi.pdf mariachi
-pdftoppm -png -r 240 -progress mariachi_repeat.pdf mariachi
+pdftoppm -png -r 48 -progress -singlefile -f 500 mariachi.pdf mariachi
+pdftoppm -png -r 240 -progress mariachi.pdf mariachi
 
 # convert to video
 ffmpeg  -ss 00:00:00 -i mariachi-%03d.png -ss 00:02:34 -i The_Mariachis_-_Feliz_Navidad_official_video-The_Mariachis-C9QHtOnEIHk.m4a -shortest mariachi_raw.mp4
